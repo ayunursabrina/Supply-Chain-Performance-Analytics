11e5ded800a23729df483722a5fb41550d3a4504
ayunursabrina <farhanaanursabrina@gmail.com>
2026-06-17 21:07:19 +0700

    Update CountryReference.java

--- src/CountryReference.java
+++ src/CountryReference.java
@@ -2,11 +2,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class CountryReference {
-
     private static final Map<String, String> countryMap = new HashMap<>();
-
     static {
-
         countryMap.put("Singapore", "Singapore");
         countryMap.put("Tokyo", "Japan");
         countryMap.put("New York", "USA");
@@ -15,9 +12,7 @@ public class CountryReference {
         countryMap.put("Sydney", "Australia");
         countryMap.put("Dubai", "UAE");
         countryMap.put("Jakarta", "Indonesia");
-
     }
-
     public static String getCountry(String city) {
         return countryMap.get(city);
     }
